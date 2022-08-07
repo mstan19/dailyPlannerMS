643636d3d5dd82420b41924566f7a9c0f6678ea6
melissastan <melissastan91@gmail.com>
2022-08-07 17:58:22 -0400

    adding the user input and saving it to LS

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,20 @@
 //console.log("ji")
+var addEvent = $("#nineAMevent");
+// var saveBtn = 
 
 var weekDay = moment().format("ddd MMM Do, YYYY");
 $("#currentDay").text(weekDay);
 
+function btnClicked(hour) {
+    var input = $("#input" + hour);
+    //console.log(input.val())
+
+saveToLS(input);
+}
+
+//cat is recieving the value of input
+
+function saveToLS(cat) {
+    var printInput = cat.val();
+    localStorage.setItem("event", printInput);
+}
