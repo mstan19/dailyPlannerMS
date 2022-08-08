e6c887641de5e509345ccee9da59cec7dcbc6be8
melissastan <melissastan91@gmail.com>
2022-08-08 17:07:21 -0400

    madde the textarea bigger

--- assets/script.js
+++ assets/script.js
@@ -48,55 +48,7 @@ for (const key in listOfTime) {
         backgroundEl.css("background-color", "#d3d3d3");
     } else {
         backgroundEl.css("background-color", "#77dd77");
-
-
     }
 }
-
-
-
-    
-    // //console.log(typeof(currentHour));
-    // var backGMain = $("<article>");
-    // console.log(backGMain);
-    // var scheduledHour = backGMain.attr("id");
-    // //console.log(scheduledHour);
-
-    //var scheduledHour = $("#14").attr("id");
-    //console.log(currentHour);
-
-
-    // var htmlHour = $("#background" + xyz);
-    // ///var htmlHour = $("#background02")
-    // var hourID = $("#background" + xyz);
-    // var hour = $("#02").attr("id");
-    // var scheduledHour =  parseInt(hour);
-    //console.log(hour);
-    // var now = moment().format("HH");
-    // var currentHour = parseInt(now);
-    //console.log(now);
-   // var row =  $(".row");
-
-    
-    //console.log(actualTime);
-    // scheduledHour = parseInt(hour);
-    //$(".row").removeClass(".present .past .future");
-    //console.log(row);
-    //console.log(scheduledHour);
-    // //console.log(typeof(currentHour));
-    // if (scheduledHour === currentHour) {
-    //     //var idBackground = $("#input" + hour);
-    //     //console.log(idBackground);
-    //     htmlHour.css("background-color", "red");
-    // }
-    // } else if (hour > currentHour) {
-    //     $(".row").addClass(".future");
-    // } else {
-    //     $(".row").addClass(".past");
-    // }
 }
 checkTime();
-
-// var currentHour = moment().hour();
-// console.log(typeof(currentHour));
-// console.log(currentHour);
