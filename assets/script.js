c5b40e6d017566235823d4d35d9da31dd9e7d1c5
melissastan <melissastan91@gmail.com>
2022-08-08 01:34:30 -0400

    working on checkTime function

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 var addEvent = $("#nineAMevent");
 // var saveBtn = 
 
-var weekDay = moment().format("ddd MMM Do, YYYY");
+var weekDay = moment().format("dddd MMM Do, YYYY");
 $("#currentDay").text(weekDay);
 
 function btnClicked(hour) {
@@ -25,3 +25,26 @@ function displayEvent () {
 } 
 
 displayEvent ();
+
+//time
+function checkTime() {
+    var hour = $("#01").attr("id");
+    var scheduledHour =  parseInt(hour);
+    //console.log(hour);
+    var now = moment().format("HH");
+    //console.log(now);
+    
+    //console.log(actualTime);
+    scheduledHour = parseInt(hour);
+    $(".row").removeClass(".present .past .future");
+   // console.log(scheduledHour);
+    //console.log(typeof(scheduledHour));
+    if (scheduledHour === now) {
+        $(".row").addClass(".present");
+    } else if (hour > now) {
+        $(".row").addClass(".future");
+    } else {
+        $(".row").addClass(".past");
+    }
+}
+checkTime();
