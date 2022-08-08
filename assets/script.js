bc3babe50279245ae03ade566f8b8f93c3d07fae
melissastan <melissastan91@gmail.com>
2022-08-08 15:48:15 -0400

    the color matches the current time

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,9 @@
-//console.log("ji")
-var addEvent = $("#nineAMevent");
-// var saveBtn = 
-
 var weekDay = moment().format("dddd MMM Do, YYYY");
 $("#currentDay").text(weekDay);
 
 function btnClicked(hour) {
     var input = $("#input" + hour);
-    //console.log(input.val())
+    //console.log(typeof(input));
 
 saveToLS(input);
 }
@@ -27,29 +23,72 @@ function displayEvent () {
 displayEvent ();
 
 //time
-function checkTime(xyz,h) {
-    var htmlHour = $("#background" + xyz);
-    ///var htmlHour = $("#background02")
-    var hour = $("#02").attr("id");
-    var scheduledHour =  parseInt(hour);
+function checkTime(xyz,generalHourID) {
+const listOfTime = {
+    background09: 9,
+    background10: 10,
+    background11: 11,
+    background12: 12,
+    background13: 13,
+    background14: 14,
+    background15: 15,
+    background16: 16,
+    background17: 17,
+};
+
+var currentHour = moment().hour();
+//$(".row").removeClass(".present .past .future");
+for (const key in listOfTime) {
+    var backgroundEl = $("#" + key)
+    if (listOfTime[key] === currentHour) {
+        //var idBackground = $("#input" + hour);
+        //console.log(idBackground);
+        backgroundEl.css("background-color", "#ff6961");
+    } else if (listOfTime[key] < currentHour) {
+        backgroundEl.css("background-color", "#d3d3d3");
+    } else {
+        backgroundEl.css("background-color", "#77dd77");
+
+
+    }
+}
+
+
+
+    
+    // //console.log(typeof(currentHour));
+    // var backGMain = $("<article>");
+    // console.log(backGMain);
+    // var scheduledHour = backGMain.attr("id");
+    // //console.log(scheduledHour);
+
+    //var scheduledHour = $("#14").attr("id");
+    //console.log(currentHour);
+
+
+    // var htmlHour = $("#background" + xyz);
+    // ///var htmlHour = $("#background02")
+    // var hourID = $("#background" + xyz);
+    // var hour = $("#02").attr("id");
+    // var scheduledHour =  parseInt(hour);
     //console.log(hour);
-    var now = moment().format("HH");
-    var currentHour = parseInt(now);
+    // var now = moment().format("HH");
+    // var currentHour = parseInt(now);
     //console.log(now);
    // var row =  $(".row");
 
     
     //console.log(actualTime);
-    scheduledHour = parseInt(hour);
+    // scheduledHour = parseInt(hour);
     //$(".row").removeClass(".present .past .future");
     //console.log(row);
     //console.log(scheduledHour);
-    //console.log(typeof(currentHour));
-    if (scheduledHour === currentHour) {
-        //var idBackground = $("#input" + hour);
-        //console.log(idBackground);
-        htmlHour.css("background-color", "red");
-    }
+    // //console.log(typeof(currentHour));
+    // if (scheduledHour === currentHour) {
+    //     //var idBackground = $("#input" + hour);
+    //     //console.log(idBackground);
+    //     htmlHour.css("background-color", "red");
+    // }
     // } else if (hour > currentHour) {
     //     $(".row").addClass(".future");
     // } else {
@@ -57,3 +96,7 @@ function checkTime(xyz,h) {
     // }
 }
 checkTime();
+
+// var currentHour = moment().hour();
+// console.log(typeof(currentHour));
+// console.log(currentHour);
